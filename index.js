a8aa18f17ab88b7906b1ca8c0a47a12ee9c0a4a9
tothricsaj <tothricsaj@gmail.com>
2023-07-14 12:40:48 +0200

    feat: added auth logic

--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const http = require('http');
-const fs = require('fs');
+const fs = require('fs/promises');
+
+function unAuth(res) {
+  res.writeHead(401, {
+    'WWW-Authenticate': 'Basic'
+  });
+  res.end();
+}
 
 //create a server object:
 http.createServer(function (req, res) {
-  fs.readFile('./index.html', (err, data) => {
-    res.writeHead(401, {
-      'WWW-Authenticate': 'Basic realm="Home"' // chellenge the client
-    });
-    const encodedAuth = req.headers.authorization || '';
+
+  try {
+    const { authorization } = req.headers;
+
+    // unAuth(res);
+    // return;
+    if(!authorization) {
+      unAuth(res)
+      return;
+    }
+
+    const encodedAuth = authorization || '';
     const encodedUserNameAndPass = encodedAuth ? encodedAuth.split(' ')[1] : '';
 
     console.log(encodedAuth);
@@ -25,15 +39,79 @@ http.createServer(function (req, res) {
           'Content-type': 'text/html',
           'Cache-Control': 'no-cache'
         });
-        res.write(data)
+        res.write('Hello Ricsi!')
         return res.end();
-      } 
+      } else {
+
+        unAuth(res);
+
+        return res.end();
+      }
     }
 
-    res.writeHead(403, {
-      'Content-type': 'text/plain'
-    });
-    res.write('403 Forbidden!');
+    res.write('Hello world!');
     return res.end();
-  });
+
+
+
+  } catch(error) {
+    console.log(error);
+    res.writeHead(500)
+    res.end
+  }
+
+
+
+
+  // if(encodedUserNameAndPass) {
+  //   const [user, pass] = Buffer.from(encodedUserNameAndPass, 'base64')
+  //     .toString('utf-8')
+  //     .split(':')
+  //   ;
+  //   console.log(user, pass);
+
+  //   if(user !== 'ricsi' && pass !== 'ricsi') {
+  //     res.writeHead(401, {
+  //       'WWW-Authenticate': 'Basic'
+  //     });
+  //     res.end();
+  //   }
+  // }
+
+ 
+  // fs.readFile('./index.html', (err, data) => {
+  //   res.writeHead(401, {
+  //     'WWW-Authenticate': 'Basic' // chellenge the client
+  //   });
+  //   const encodedAuth = req.headers.authorization || '';
+  //   const encodedUserNameAndPass = encodedAuth ? encodedAuth.split(' ')[1] : '';
+
+  //   console.log(encodedAuth);
+  //   console.log(encodedUserNameAndPass);
+
+  //   if(encodedUserNameAndPass) {
+  //     const [user, pass] = Buffer.from(encodedUserNameAndPass, 'base64')
+  //       .toString('utf-8')
+  //       .split(':')
+  //     ;
+  //     console.log(user, pass);
+
+  //     if(user === 'ricsi' && pass === 'password') {
+  //       res.writeHead(200, {
+  //         'Content-type': 'text/html',
+  //         'Cache-Control': 'no-cache'
+  //       });
+  //       res.write(data)
+  //       return res.end();
+  //     } 
+
+  //     return res.end();
+  //   }
+
+  //   res.writeHead(403, {
+  //     'Content-type': 'text/plain'
+  //   });
+  //   res.write('403 Forbidden!');
+  //   return res.end();
+  // });
 }).listen(8080); //the server object listens on port 8080
